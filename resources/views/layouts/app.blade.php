d9a53a45e8efa1ef2882634d67af87dc2285dff2
margaretmwaura <mwauramargaret1@gmail.com>
2019-11-14 10:09:49 +0300

    proper nav-bar creation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,9 +53,6 @@
                           <li class="nav-item">
                             <a class="nav-link" href="/tbapp/public/about">About</a>
                           </li>
-                          <li class="nav-item">
-                            <a class="nav-link" href="/tbapp/public/createdevents/create">{{ app('App\Http\Controllers\UserNumberController')->number_of_users() }}</a>
-                          </li>
                           <li class="nav-item active">
                             <a class="nav-link" href="/tbapp/public/createdevents">Events</a>
                           </li>
